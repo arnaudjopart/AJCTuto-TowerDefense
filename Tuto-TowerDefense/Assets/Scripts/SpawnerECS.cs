cc800844f57e6702ac5881178dfab22989e1fa8f
arnaudjopart <arnaudjopart@users.noreply.github.com>
2019-11-06 22:15:40 +0100

    Create Instance ECS

---
+++ Tuto-TowerDefense/Assets/Scripts/SpawnerECS.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class SpawnerECS : MonoBehaviour
+{
+    public GameObject m_turretPrefab;
+
+    public GameObject m_tankPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_entityManager = World.Active.EntityManager;
+        CreateTurretAt(Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Entity CreateEntityFromPrefab(GameObject _prefab, Vector3 _spawnPosition)
+    {
+        var ECSPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(_prefab, World.Active);
+        var instanceECS = m_entityManager.Instantiate(ECSPrefab);
+        
+        m_entityManager.SetComponentData(instanceECS, new Translation{Value = _spawnPosition});
+
+        return instanceECS;
+    }
+
+    public void CreateTurretAt(Vector3 _spawnPosition)
+    {
+        Entity instance = CreateEntityFromPrefab(m_turretPrefab, _spawnPosition);
+    }
+
+    public void CreateTankAt(Vector3 _spawnPosition)
+    {
+        Entity instance = CreateEntityFromPrefab(m_tankPrefab, _spawnPosition);
+    }
+
+    private EntityManager m_entityManager;
+}
+
+
+
+
+
