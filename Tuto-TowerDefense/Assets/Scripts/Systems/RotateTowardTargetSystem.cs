52b8d9a6ed0b7a83a4cbd71c5f43fe9821929f50
arnaudjopart <arnaudjopart@users.noreply.github.com>
2019-12-03 23:59:26 +0100

    Adapting RotateTowardSystem

--- Tuto-TowerDefense/Assets/Scripts/Systems/RotateTowardTargetSystem.cs
+++ Tuto-TowerDefense/Assets/Scripts/Systems/RotateTowardTargetSystem.cs
@@ -10,7 +10,9 @@ public class RotateTowardTargetSystem : ComponentSystem
     protected override void OnUpdate()
     {
         ComponentDataFromEntity<Translation> translations = GetComponentDataFromEntity<Translation>();
+        ComponentDataFromEntity<LocalToWorld> ltws = GetComponentDataFromEntity<LocalToWorld>();
         
+        //Case when rotating entity is not a child
         Entities.ForEach((Entity _entity, ref RotationTowardTargetComponent _rotationTowardTarget, ref Translation
             _translation, ref TargetComponent _targetComponent, ref Rotation _rotation) =>
         {
@@ -28,5 +30,39 @@ public class RotateTowardTargetSystem : ComponentSystem
             _rotation.Value = Quaternion.Lerp(_rotation.Value, targetRotation, _rotationTowardTarget
             .m_rotationSpeed*Time.deltaTime);
         });
+        
+        //Case when rotating entity is child of another entity
+        Entities.ForEach((Entity _entity, ref RotationTowardTargetComponent _rotationTowardTarget, ref Translation
+            _translation, ref Rotation _rotation, ref LocalToWorld _localToWorld, ref Parent _parent) =>
+        {
+            if (EntityManager.Exists(_rotationTowardTarget.m_target) &&
+                EntityManager.HasComponent<Translation>(_rotationTowardTarget.m_target))
+            {
+                float3 targetDirectionInWorldSpace = translations[_rotationTowardTarget.m_target].Value -
+                                                     _localToWorld.Position;
+                Quaternion targetRotationInWorldSpace = Quaternion.LookRotation(targetDirectionInWorldSpace);
+
+                LocalToWorld parentLTW = ltws[_parent.Value];
+                quaternion parentWorldRotation = Quaternion.LookRotation(parentLTW.Forward, parentLTW.Up);
+
+                quaternion relativeRotationOfTurret =
+                    math.mul(targetRotationInWorldSpace, math.inverse(parentWorldRotation));
+                
+                _rotation.Value = Quaternion.Lerp(_rotation.Value, relativeRotationOfTurret, _rotationTowardTarget
+                .m_rotationSpeed*Time.deltaTime);
+            }
+        });
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
